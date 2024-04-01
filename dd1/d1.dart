02b36053e841f16d3d6c071120b633078cd36b5a
BernardoPeixer <bernardo.peixer.stahelin@gmail.com>
2024-04-01 13:55:27 -0300

    D1

--- dd1/d1.dart
+++ dd1/d1.dart
@@ -1,3 +1,11 @@
+import 'dart:math';
+
 void main() {
-  
+  List<double> raios = [5, 8, 12, 7.3, 18, 2, 25];
+
+  for(int i = 0; i < raios.length; i++) {
+    print("Raio: ${raios[i]}, Área: ${(pi * raios[i] * raios[i]).toStringAsFixed(2)}, Perímetro: ${(2 * pi * raios[i]).toStringAsFixed(2)}");
+  }
 }
+
+// Feito em 5:54,70

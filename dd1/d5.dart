dd6b721a174b2b1fb1865c058b0893595db7262a
BernardoPeixer <bernardo.peixer.stahelin@gmail.com>
2024-04-01 17:32:25 -0300

    D5

--- dd1/d5.dart
+++ dd1/d5.dart
@@ -1,3 +1,36 @@
+import 'dart:math';
+
 void main() {
-  
+
+  Random random = Random();
+
+  List<String> nomes = ["Ana", "Francisco","Joao",
+  "Pedro", "Gabriel", "Rafaela",
+  "Marcio", "Jose", "Carlos",
+  "Patricia", "Helena", "Camila",
+  "Mateus", "Gabriel", "Maria",
+  "Samuel", "Karina","Antonio",
+  "Daniel", "Joel", "Cristiana",
+  "Sebastião", "Paula"];
+
+  List<String> sobrenomes = ["Silva", "Ferreira", "Almeida",
+  "Azevedo", "Braga", "Barros",
+  "Campos", "Cardoso", "Teixeira",
+  "Costa", "Santos", "Rodrigues",
+  "Souza", "Alves", "Pereira",
+  "Lima", "Gomes", "Ribeiro",
+   "Carvalho", "Lopes", "Barbosa"];
+
+  int indiceAleatorioNomes = random.nextInt(nomes.length);
+  int indiceAleatorioSobrenomes = random.nextInt(sobrenomes.length);
+
+  String nomeAleatorio = nomes[indiceAleatorioNomes];
+  String sobrenomeAleatorio = sobrenomes[indiceAleatorioSobrenomes];
+
+  print("Nome: $nomeAleatorio $sobrenomeAleatorio");
 }
+
+// Finalizado em 10:01,26
+
+
+
